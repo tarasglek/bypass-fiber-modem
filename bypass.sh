875efbc0ff51dbc79bfbf31204dc3e60628035a7
Taras Glek <taras@glek.net>
2020-09-04 21:14:53 -0700

    keep forwarding auth to att box

--- bypass.sh
+++ bypass.sh
@@ -11,6 +11,8 @@ ethtool -P wan0 | awk '{{print $3}}' | xargs ip link set wan0 address
 # creating a bridge
 ip link add name br0 type bridge
 ip link set br0 up
+ip link del br0.0
+ip link add link br0 name br0.0 type macvlan
 ip link set wan0 up
 ip link set bridge0 up
 #ip link set enp4s0 up
@@ -21,5 +23,10 @@ ip link set bridge0 master br0
 ip link set br0 type bridge vlan_filtering 1
 # this part is important. Without it the 802.1x EAP packets are not forwarded. Default behavior states to not forward auth traffic through a bridge
 echo 8 > /sys/class/net/br0/bridge/group_fwd_mask
-cd `dirname $0` && ./.venv/bin/python steal_dhcp.py wan0 br0 |sh
-ip link set bridge0 down
+# https://www.haught.org/2018/04/13/att-router-bypass/
+# forward auth traffic to att box
+ebtables -t filter -A FORWARD -i bridge0 -p 802_1Q --vlan-encap 0x888e -j ACCEPT
+ebtables -t filter -A FORWARD -i bridge0 -p 802_1Q -j DROP
+ebtables -t filter -A FORWARD -o bridge0 -p 802_1Q --vlan-encap 0x888e -j ACCEPT
+ebtables -t filter -A FORWARD -o bridge0 -p 802_1Q -j DROP
+cd `dirname $0` && ./.venv/bin/python steal_dhcp.py wan0 br0.0 |sh
--- steal_dhcp.py
+++ steal_dhcp.py
@@ -2,7 +2,7 @@
 from scapy.all import *
 import sys
 
-def main(wan, br):
+def main(sniff_eth, wan):
     def arp_monitor_callback(pkt):
         if DHCP in pkt:
             info = {}
@@ -15,15 +15,14 @@ def main(wan, br):
             netmask = info['subnet_mask']
             mac = pkt[Ether].dst
             print(("set -x\n"
-                f"ip link del {br}\n"
                 f"ifconfig {wan} {ip} netmask {netmask}\n"
                 f"ip route add default via {router}\n"
-                f"ip link set wan0 address {mac}\n"
+                f"ip link set {wan} address {mac}\n"
             ))
             sys.exit(0)
     # reduce python overhead by only scanning packets that are dhcp or have vlan tag
     # shoud be very few of these
-    sniff(prn=arp_monitor_callback, iface=wan, store=0)
+    sniff(prn=arp_monitor_callback, iface=sniff_eth, store=0)
     # sniff(offline="foo.pcap", prn=arp_monitor_callback,filter="vlan or (udp and (port 67 or 68))", store=0 )
 
 if __name__ == "__main__":
